5c2104a188eb400008587ff243123666ddfd1a13
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2019-11-08 16:40:19 +0300

    General view of the chat

---
+++ PizzaChat/PizzaChat/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PizzaChat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSendMsg = new System.Windows.Forms.Button();
+            this.fldDialogBox = new System.Windows.Forms.RichTextBox();
+            this.fldMsgBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btSendMsg
+            // 
+            this.btSendMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSendMsg.Location = new System.Drawing.Point(194, 418);
+            this.btSendMsg.Name = "btSendMsg";
+            this.btSendMsg.Size = new System.Drawing.Size(75, 23);
+            this.btSendMsg.TabIndex = 0;
+            this.btSendMsg.Text = "Отпавить";
+            this.btSendMsg.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btSendMsg.UseVisualStyleBackColor = true;
+            // 
+            // fldDialogBox
+            // 
+            this.fldDialogBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fldDialogBox.Location = new System.Drawing.Point(12, 14);
+            this.fldDialogBox.Name = "fldDialogBox";
+            this.fldDialogBox.Size = new System.Drawing.Size(256, 398);
+            this.fldDialogBox.TabIndex = 1;
+            this.fldDialogBox.Text = "";
+            // 
+            // fldMsgBox
+            // 
+            this.fldMsgBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fldMsgBox.Location = new System.Drawing.Point(13, 418);
+            this.fldMsgBox.Name = "fldMsgBox";
+            this.fldMsgBox.Size = new System.Drawing.Size(175, 22);
+            this.fldMsgBox.TabIndex = 2;
+            this.fldMsgBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btSendMsg;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 453);
+            this.Controls.Add(this.fldMsgBox);
+            this.Controls.Add(this.fldDialogBox);
+            this.Controls.Add(this.btSendMsg);
+            this.Name = "MainForm";
+            this.Text = "Chat pizza";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSendMsg;
+        private System.Windows.Forms.RichTextBox fldDialogBox;
+        private System.Windows.Forms.RichTextBox fldMsgBox;
+    }
+}
+
---
+++ PizzaChat/PizzaChat/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaChat
+{
+    public interface IMainForm
+    {
+        string DialogBox { get; }
+        string MsgBox { get; set; }
+        //void SendMsg(string Msg);
+        event EventHandler SendMsg;
+    }
+    public partial class MainForm : Form, IMainForm
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+            btSendMsg.Click += new EventHandler(btSendMsg_Click);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            fldDialogBox.Text += fldMsgBox.Text;
+            fldMsgBox.Text = String.Empty;
+
+        }
+
+        void btSendMsg_Click(object sender, EventArgs e)
+        {
+            if (fldMsgBox.Text.Length > 0)
+            {
+                fldDialogBox.Text = fldDialogBox.Text + "\n" + fldMsgBox.Text;
+                fldMsgBox.Text = String.Empty;
+            }
+            //this.txtBlock.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        public string DialogBox
+        {
+            get { return fldDialogBox.Text; }
+        }
+        public string MsgBox
+        {
+            set {fldMsgBox.Text = value; }
+            get { return fldMsgBox.Text;}
+        }
+
+
+        public event EventHandler SendMsg;
+    }
+}
