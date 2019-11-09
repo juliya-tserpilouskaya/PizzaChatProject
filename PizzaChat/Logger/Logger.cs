7a288c8dba7d3b6881844fb415ab538149922394
Julija Julija <juliet_ai@mail.ru>
2019-11-10 00:46:53 +0300

    +add logger project branch *maked logger test structure

---
+++ PizzaChat/Logger/Logger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public interface ILogger
+    {
+        void INFOLog(string message);
+        void ERRORLog(string message);
+        void WARNLog(string message);
+        void PersonsLog(OurPerson[] persons);
+        
+    }
+    public class Logger : ILogger
+    {
+        public Logger()
+        {
+
+        }
+        public void INFOLog(string message)
+        {
+            //TODO: запись в файл сделать
+            Console.WriteLine(message);
+        }
+        public void ERRORLog(string message)
+        {
+            //TODO: запись в файл сделать
+            Console.WriteLine(message);
+        }
+        public void WARNLog(string message)
+        {
+            //TODO: запись в файл сделать
+            Console.WriteLine(message);
+        }
+        public void PersonsLog(OurPerson[] persons)
+        {
+            throw new NotImplementedException();
+            //Concept in development
+        }
+    }
+    #region Persons class example
+    public class OurPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    #endregion
+}
